ee80c4219d3165a66baff8a10c88a1f9b711841b
orinbai <orinbai@sina.com>
2015-11-26 19:58:56 +0800

    aa

--- Robot/test2.py
+++ Robot/test2.py
@@ -1,3 +1,14 @@
-import os
-filelist = map(lambda x: x.split('.')[0], filter(lambda x: x.endswith('intro'), os.listdir('tmpfile')))
+a = '123a123a123a123a123a'
 
+def aaaa(x, y=0):
+    #y = int(y)
+    print y
+    if x.find('a', y+1) < 0:
+        return str(-1)
+    else:
+        print "**"*5
+        m = x.find('a', y+1)
+        #return list(str(m)) + list(aaaa(a, m))
+        return str(m) + ',' + aaaa(a, m)
+
+print aaaa(a).split(',')
--- Robot/txtclass.py
+++ Robot/txtclass.py
@@ -1,7 +1,14 @@
 # -*- coding: utf8 -*-
 import jieba, re
 
-chRE = re.compile(u"[\u4e00-\u9fa5, a-z, 0-9]")
+MAXLEN = 5
+IGSIGN = {',':1, '.':1, '-':1, ';':1, '?':1, ':':1, '"':1, '（':1, '）':1}
+#chRE = re.compile(u"[\u4e00-\u9fa5, a-z, 0-9]")
+### For NewWord, So Eliminate English Char ###
+chRE = re.compile(u"[\u4e00-\u9fa5]")
+
+totalStr = ''
+uniqCHAR = {}
 
 def F2H(cstring):
     tstring = ''
@@ -14,16 +21,78 @@ def F2H(cstring):
         tstring += unichr(inside_code)
     return tstring
 
-a = '摩卡世界是一家专业的手机联网游戏开发公司，成立于2 0 0 7年'
-print ' '.join(chRE.findall(a.decode('utf8'))).encode('utf8')
-exit()
+##### inEfficiency Using Recursion #####
+#def enumerateWD(tCopus, sChar, sSign=0):
+#    tmparr = []
+#    sSign = int(sSign)
+#    if tCopus.find(sChar, sSign+1) < 0:
+#        return [-1]
+#    else:
+#        m = tCopus.find(sChar, sSign+1)
+#        tmparr.extend([m])
+#        tmparr.extend(enumerateWD(tCopus, sChar, m))
+#        return tmparr
+#########################################
+
+def enumerateWD(tCopus, sChar):
+    tmparr = []
+    bgSign = 0
+    edSign = len(tCopus)
+    m = tCopus.find(sChar, 0)
+    while m >= 0 :
+        yield m
+        #tmparr.append(m)
+        m = tCopus.find(sChar, m+1)
+
+    
+
+#print enumerateWD(a, '家')
+#print a[104:107]
+#exit()
+
 f = open('result/intro.txt')
 for lines in f:
     line = lines.decode('utf8').strip().split('\t')
-    if line[1] != 'False':
-        print line[1].encode('utf8')
-        line[1] = F2H(line[1])
-        print '/ '.join(jieba.cut(line[1], cut_all=False)).encode('utf8')
-        print '---'*30 
-
+    line[1] = line[1].strip()
+    if line[1] != 'False' or line[1]:
+        m = filter(lambda x: not IGSIGN.has_key(x), chRE.findall(line[1]))
+        #print '\n'.join(m).encode('utf8').strip()
+        totalStr += ''.join(m).strip()
+        for wd in m:
+            if uniqCHAR.has_key(wd): continue
+            uniqCHAR[wd] = 1
+        ########### Check Punctuation ##############
+        #print chRE.sub('', line[1]).encode('utf8')#
+        ############################################
+    else:
+        continue
 f.close()
+
+for sKey in sorted(uniqCHAR.keys()):
+    if not sKey.strip(): continue
+    tmparr = []
+    print sKey.encode('utf8')
+    for i in enumerateWD(totalStr, sKey):
+        #tmparr.append(totalStr[i-1:i+5].encode('utf8'))
+        tmparr.append(totalStr[i-1:i+5])
+
+    print tmparr[0][0:2].encode('utf8')
+    print '--'*10
+    print '\n'.join(sorted(tmparr, key=lambda x: x[1:])).encode('utf8')
+
+#print len(totalStr)
+#print totalStr[641112:641112+5].encode('utf8')
+#print enumerateWD(totalStr, '公'.decode('utf8'))
+
+#print totalStr.encode('utf8')
+#print ' '.join(uniqWd.keys()).encode('utf8'), len(uniqWd.keys())
+#f = open('result/intro.txt')
+#for lines in f:
+#    line = lines.decode('utf8').strip().split('\t')
+#    if line[1] != 'False':
+#        print line[1].encode('utf8')
+#        line[1] = F2H(line[1])
+#        print '/ '.join(jieba.cut(line[1], cut_all=False)).encode('utf8')
+#        print '---'*30 
+#
+#f.close()
